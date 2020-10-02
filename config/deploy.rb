230e115dec536c421040dcb464d4dd65da7b8d3a
Javinto <info@javinto.nl>
2020-10-02 22:33:43 +0200

    Add masterkey

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :deploy_via, :remote_cache
 set :copy_exclude, [ '.git' ]
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
